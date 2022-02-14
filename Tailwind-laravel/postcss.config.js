802650f82821c4e6240abd778b633f6da223b9e5
k.kawano <k-kawano@imt-mirai.co.jp>
2022-02-14 21:39:30 +0900

    アップデート（config動かない

--- Tailwind-laravel/postcss.config.js
+++ Tailwind-laravel/postcss.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-    plugins: [require('postcss-import'), require('postcss-nested')],
+    // plugins: [require('postcss-import'), require('postcss-nested')],
+    plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+        'postcss-import': {},
+        'postcss-nested': {},
+    },
 };
--- Tailwind-laravel/tailwind.config.js
+++ Tailwind-laravel/tailwind.config.js
@@ -1,13 +1,42 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
-    mode: 'jit',
-    enabled: true,
-    purge: ['./resources/views/**/*.{js,vue,blade.php,php}'],
+    content: ['./resources/views/**/*.{js,vue,blade.php,php}'],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {},
+        colors: {
+            bland: 'var(--color-bland)',
+            // defaultThemeを使うときはmy-を付けて両方活かす
+            gray: {
+                1: '#eee',
+                2: '#333',
+                3: '#666',
+            },
+            red: {
+                1: '#dc3545',
+            },
+            blue: {
+                1: '#283b64',
+            },
+            // ...defaultTheme.colors,
+            // defaultThemeを消したときに使うやつ
+            transparent: 'transparent',
+            current: 'currentColor',
+            white: '#fff',
+        },
+        fontFamily: {
+            roboto: 'var(--font-roboto)',
+        },
+        screens: {
+            xs: {
+                max: '374px',
+            },
+            ...defaultTheme.screens,
+        },
     },
     variants: {
         extend: {},
+        lineClamp: ['responsive'],
     },
     plugins: [require('@tailwindcss/line-clamp'), require('@tailwindcss/forms')],
 };
